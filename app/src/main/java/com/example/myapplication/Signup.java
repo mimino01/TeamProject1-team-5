a0bf64454c6335df56d6e5c0ea324eb8e6c85108
DESKTOP-TRPE8TE\mimin <mimino0144@gmail.com>
2022-10-07 12:31:09 +0900

    activity_main 오류 수정

--- app/src/main/java/com/example/myapplication/Signup.java
+++ app/src/main/java/com/example/myapplication/Signup.java
@@ -1,4 +1,24 @@
 package com.example.myapplication;
 
-public class Signup {
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Signup extends AppCompatActivity {
+    TextView sign;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sign = findViewById(R.id.signin);
+
+        sign.setOnClickListener(v -> {
+            Intent intent = new Intent(this, Signup.class);
+            startActivity(intent);
+        });
+    }
 }
