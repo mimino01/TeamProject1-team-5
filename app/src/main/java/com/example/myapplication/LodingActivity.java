e989d8d3a5778fa53053ad468f37a3df62d0058b
DESKTOP-TRPE8TE\mimin <mimino0144@gmail.com>
2022-10-13 18:08:34 +0900

    로딩화면 버그픽스

--- app/src/main/java/com/example/myapplication/LodingActivity.java
+++ app/src/main/java/com/example/myapplication/LodingActivity.java
@@ -1,2 +1,26 @@
-package com.example.myapplication;public class LodingActivity {
+package com.example.myapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LodingActivity extends AppCompatActivity {
+    TextView sign;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loding);
+
+        sign = findViewById(R.id.textView);
+
+        sign.setOnClickListener(view -> {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+    }
 }
--- app/src/main/java/com/example/myapplication/Signup.java
+++ app/src/main/java/com/example/myapplication/Signup.java
@@ -12,7 +12,7 @@ public class Signup extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_signup);
 
         sign = findViewById(R.id.signin);
 
