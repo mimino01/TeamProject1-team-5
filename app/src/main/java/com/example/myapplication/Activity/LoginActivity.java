c1ae4eb598fc4f17c8bccc0aa9161d2fa7da07d8
DESKTOP-TRPE8TE\mimin <mimino0144@gmail.com>
2022-10-16 18:08:03 +0900

    로그인 구현

--- app/src/main/java/com/example/myapplication/Activity/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/Activity/LoginActivity.java
@@ -1,8 +1,12 @@
 package com.example.myapplication.Activity;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,6 +14,7 @@ import com.example.myapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
     TextView id, password, login, logup;
+    boolean loginCorrect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +26,20 @@ public class LoginActivity extends AppCompatActivity {
         login = findViewById(R.id.signin);
         logup = findViewById(R.id.signup);
 
+        //임시데이터
+         loginCorrect = true;
+
         logup.setOnClickListener(view -> {
             Intent intent = new Intent(this, SignupActivity.class);
             startActivity(intent);
         });
 
-
+        login.setOnClickListener(view -> {
+            if (loginCorrect) {
+                //임시로 로딩화면 이동으로 함 추후 메인화면 구현시 변경요망
+                Intent intent = new Intent(this, LodingActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
