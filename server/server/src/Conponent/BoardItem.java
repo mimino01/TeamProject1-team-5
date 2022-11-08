df98daacb5ca7e8e73cff4ae08dee5f2ebd93ec6
DESKTOP-TRPE8TE\mimin <mimino0144@gmail.com>
2022-11-08 19:04:45 +0900

    보드 수정중

--- server/server/src/Conponent/BoardItem.java
+++ server/server/src/Conponent/BoardItem.java
@@ -1,23 +1,28 @@
 package Conponent;
 
 public class BoardItem {
-    public String[] user;
+    final int MAXUSER = 4;
+    public String[] user = new String[MAXUSER];
     public String host;
-    public Option userData;
-    public Option hostData;
     public int numberOfUser;
     public String destination;
     public float departureTime;
+    public Double[] coordinate = new Double[2];
 
+    //초기화
     public BoardItem() {
     }
 
-    public BoardItem(String host, String destination, float departureTime) {
+    //방장 아이디, 방장 위치, 목적지, 출발시각 입력받음
+    public BoardItem(String host, String destination, float departureTime, Double Latitude, Double longitude) {
         this.host = host;
         this.destination = destination;
         this.departureTime = departureTime;
+        this.coordinate[0] = Latitude;
+        this.coordinate[1] = longitude;
     }
 
+    //유저 추가
     public boolean addUser(String id) {
         if (numberOfUser > 4) {
             return false;
@@ -28,11 +33,21 @@ public class BoardItem {
         }
     }
 
+    //유저맴버 정보 반환
     public String[] getUser() {
         return user;
     }
 
-    public void setUser(String[] user) {
-        this.user = user;
+    public String[][] getBoard() {
+        String[][] req = new String[10][MAXUSER];
+
+        req[0][0] = this.host;
+        req[1] = this.user;
+        req[2][0] = Double.toString(this.coordinate[0]);
+        req[2][1] = Double.toString(this.coordinate[1]);
+        req[3][0] = destination;
+        req[4][0] = Float.toString(departureTime);
+
+        return req;
     }
 }
--- server/server/src/Conponent/BoardLinkedList.java
+++ server/server/src/Conponent/BoardLinkedList.java
@@ -3,5 +3,81 @@ package Conponent;
 public class BoardLinkedList {
     class Node{
         private String data;
+        public Node link;
+
+        public Node() {
+            this.data = null;
+            this.link = null;
+        }
+
+        public Node(String data) {
+            this.data = data;
+            this.link = null;
+        }
+
+        public Node(String data, Node front) {
+            this.data = data;
+            this.link = front;
+        }
+
+        public String getData() {
+            return this.data;
+        }
+    }
+
+    public class LinkedList {
+        private Node head;
+
+        public LinkedList() {
+            head = null;
+        }
+
+        public void insertNode (Node preNode, String data) {
+            Node newNode = new Node(data);
+            newNode.link = preNode.link;
+            preNode.link = newNode;
+        }
+
+        public void insertNode (String data) {
+            if (head == null) {
+                this.head = new Node(data);
+            } else {
+                Node tempNode = head;
+                while (tempNode.link != null) {
+                    tempNode = tempNode.link;
+                }
+                tempNode.link = new Node(data);
+            }
+        }
+
+        public void deleteNode(String data) {
+            Node preNode = head;
+            Node tempNode = head.link;
+
+            if (data.equals(preNode.getData())) {
+                head = preNode.link;
+                preNode.link = null;
+            } else {
+                
+            }
+        }
+
+        public void deleteLastNode() {
+            Node preNode;
+            Node tempNode;
+            if (head == null) {
+            } else if (head.link == null) {
+                head = null;
+            } else {
+                preNode = head;
+                tempNode = head.link;
+
+                while(tempNode.link != null) {
+                    preNode = tempNode;
+                    tempNode = tempNode.link;
+                }
+                preNode.link = null;
+            }
+        }
     }
 }
